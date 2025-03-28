e305aeefcf30f310270e0d4a1c537c12d0c2a732
Salman Alfarizi <salmanlfaa11@gmail.com>
2025-03-28 22:48:45 +0700

    model question

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    protected $table = 'questions';
+    protected $primaryKey = 'idquestions';
+
+    protected $fillable = [
+        'babak',
+        'type',
+        'question',
+        'pilihan_a',
+        'pilihan_b',
+        'pilihan_c',
+        'pilihan_d',
+        'correct_answer',
+    ];
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany(QuizAnswer::class, 'question_id');
+    }
 }
