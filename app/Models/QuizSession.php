b8b2eab0cb9a90b9295ed966605e2d38e9e5e6c5
Salman Alfarizi <salmanlfaa11@gmail.com>
2025-03-28 22:49:37 +0700

    model quizSession

---
+++ app/Models/QuizSession.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class QuizSession extends Model
+{
+    protected $table = 'quiz_sessions';
+    protected $primaryKey = 'idquiz_sessions';
+
+    protected $fillable = [
+        'user_id',
+        'babak',
+        'start_time',
+        'end_time',
+        'duration',
+        'skor',
+        'warning_count',
+        'status',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany(QuizAnswer::class, 'session_id');
+    }
+}
