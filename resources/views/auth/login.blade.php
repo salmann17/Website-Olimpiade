42ee230a40518c4b279213d309403b35ff771ce3
Salman Alfarizi <salmanlfaa11@gmail.com>
2025-04-09 23:58:08 +0700

    update ui login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,14 +23,14 @@
     <form action="{{ route('login.submit') }}" method="POST" class="text-left space-y-4">
       @csrf
       <div>
-        <label for="username" class="block mb-1">Username</label>
+        <label for="username" class="block mb-1 text-[#004a80]">Username</label>
         <input type="text" name="username" id="username" required
-          class="w-full p-2 rounded-lg bg-white text-gray-800 focus:outline-none">
+          class="w-full p-2 rounded-lg bg-slate-400 text-gray-800 focus:outline-none">
       </div>
       <div>
-        <label for="password" class="block mb-1">Password</label>
+        <label for="password" class="block mb-1 text-[#004a80]" >Password</label>
         <input type="password" name="password" id="password" required
-          class="w-full p-2 rounded-lg bg-white text-gray-800 focus:outline-none">
+          class="w-full p-2 rounded-lg bg-slate-400 text-gray-800 focus:outline-none">
       </div>
       <button type="submit" class="w-full py-2 bg-[#004a80] text-[#ffffff] hover:text-[#dcdcdc] font-semibold rounded-lg hover:bg-gray-600 transition">Login</button>
     </form>
